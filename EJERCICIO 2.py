513d5a5703ff40c105ca25c31418db9f2002f18c
ybermudcas <ybermudcas@educacion.navarra.es>
2023-12-15 19:06:55 +0100

    EJERCICIO 2

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -1,12 +1,21 @@
-n = int(input("Escribe un número entre el 1 y 10"))
-
 import os
+def leer_fichero(n):
+    """función que pida un número entero entre 1 y 10, lea el fichero tabla-n.txt con la tabla de multiplicar de ese número.
 
+    Parámetros:
+        -n = el número introducido de la que se hará la tabla
 
-if os.path.isfile("tabla-" + str(n) + ".txt"):
-    fichero = open("tabla-" + str(n) + ".txt", "r")
-    fichero_leer = fichero.read()
-    print(fichero_leer)
+    Salida:
+        -Tabla de multiplicar completa
+        -Si el fichero no existe muestra un mensaje por pantalla informandolo
+    """
+    if os.path.isfile("tabla-" + str(n) + ".txt"):
+        fichero = open("tabla-" + str(n) + ".txt", "r")
+        fichero_leer = fichero.read()
+        print(fichero_leer)
 
-else:
-    print("El fichero ", n, " no existe")
+    else:
+        print("El fichero ", n, " no existe")
+    
+leer_fichero(5)
+leer_fichero(11)
