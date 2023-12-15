fcc8a7a9b121dbe67d5e5a61169b8159af686780
ybermudcas <ybermudcas@educacion.navarra.es>
2023-12-15 19:06:46 +0100

    EJERCICIO 1

--- EJERCICIO 1.py
+++ EJERCICIO 1.py
@@ -1,9 +1,9 @@
-n = int(input("Intruce un número entero entre 1 y 10"))
-fichero = open("tabla-" + str(n) + ".txt", "w")
-
-for i in range(0, 11):
-    multiplicación = n * i
-    fichero.write(str(n) + "*" + str(i) + "=" + str(multiplicación) + '\n') 
-    
-   # print(n, "*", i, "=", multiplicación)
-
+def tabla_multiplicar(n):
+    n = input("Escribe un número del 0 al 10")
+    fichero = open("tabla-" + str(n) + ".txt", "w")
+    for i in range(0, 11):
+        multiplicacion = n * i
+        fichero.write(str(n) + "*" + str(i) + "=" + str(multiplicacion) + '\n')
+        print(n, "*", i, "=", multiplicacion)
+        
+#tabla_multiplicar(10)
