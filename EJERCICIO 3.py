75df7fbc88528e38fadea9c82da06919eb5f2722
ybermudcas <ybermudcas@educacion.navarra.es>
2023-12-15 19:07:05 +0100

    EJERCICIO 3

--- EJERCICIO 3.py
+++ EJERCICIO 3.py
@@ -1,11 +1,23 @@
-n = int(input("Escribe un número entre el 1 y 10"))
-m = int(input("Escribe un número entre el 1 y 10"))
-
 import os
-if os.path.isfile("tabla-" + str(n) + ".txt"):
-    fichero = open("tabla-" + str(n) + ".txt", "r")
-    fichero_leer = fichero.readlines()
-    print(fichero_leer[m])
+def línea_tabla(n, m):
+    """Escribir una función que pida dos números n y m entre 1 y 10, lea el fichero tabla-n.txt con la
+      tabla de multiplicar de ese número, y muestre por pantalla la línea m del fichero.
 
-else:
-    print("El fichero ", n, " no existe")
+    Parámetros:
+        -n = el número introducido de la que se hará la tabla
+        -m = número por el que se va a multiplicar
+
+    Salida:
+        -línea m del fichero
+        -Si el fichero no existe muestra un mensaje por pantalla informandolo
+    """
+    if os.path.isfile("tabla-" + str(n) + ".txt"):
+        fichero = open("tabla-" + str(n) + ".txt", "r")
+        fichero_leer = fichero.readlines()
+        print(fichero_leer[m])
+
+    else:
+        print("El fichero ", n, " no existe")
+
+línea_tabla(5, 3)
+línea_tabla(11, 4)
